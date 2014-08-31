00dfcc20ebd51cb82bf049e2bce0cfb1b66b60fe
Marko Heijnen <info@markoheijnen.com>
2014-08-31 17:03:52 +0200

    Have a way to have it truly fullscreen or not.

--- fullscreen-gallery.php
+++ fullscreen-gallery.php
@@ -10,10 +10,15 @@
  */
 
 class Fullscreen_gallery {
+	private static $config = array(
+		'fullscreen' => true,
+	);
 
 	public function __construct() {
 		add_action( 'init', array( $this, 'makeplugins_add_fullscreen_endpoint' ) );
-		add_filter( 'template_include', array( $this, 'makeplugins_fullscreen_template_redirect' ) );
+
+		add_filter( 'template_redirect', array( $this, 'hide_admin_bar' ), -1 );
+		add_filter( 'template_include', array( $this, 'makeplugins_fullscreen_template_redirect' ), 0 );
 	}
 
 
@@ -21,6 +26,17 @@ class Fullscreen_gallery {
 		add_rewrite_endpoint( 'fullscreen', EP_PERMALINK );
 	}
 
+
+	public function hide_admin_bar() {
+		global $wp_query;
+ 
+		// if this is not a request for json or a singular object then bail
+		if ( ! isset( $wp_query->query_vars['fullscreen'] ) || ! is_singular() ) {
+			return $template;
+		}
+
+		add_filter( 'show_admin_bar', '__return_false' );
+	}
 
 	public function makeplugins_fullscreen_template_redirect( $template ) {
 		global $wp_query;
@@ -39,6 +55,7 @@ class Fullscreen_gallery {
 		return dirname( __FILE__ ) . '/templates/fullscreen.php';
 	}
 
+
 	public function add_superslides() {
 		wp_enqueue_style( 'superslides', plugins_url( 'css/superslides.css', __FILE__ ), array(), '0.6.2' );
 		wp_enqueue_script( 'superslides', plugins_url( 'js/jquery.superslides.min.js', __FILE__ ), array( 'jquery' ), '0.6.2' );
@@ -51,6 +68,51 @@ class Fullscreen_gallery {
 		}
 
 		return $gallery;
+	}
+
+
+	public static function get_header() {
+		if ( ! self::$config['fullscreen'] ) {
+			get_header();
+			return;
+		}
+
+		ob_start();
+		get_header();
+		$header = ob_get_contents();
+
+		ob_end_clean();
+
+		$elements = explode( '</head>', $header );
+
+		$header  = $elements[0];
+		$header .= '<style>html { border: 0px;}</style/>';
+		$header .= '</head>';
+
+		echo $header;
+	}
+
+	public static function get_footer() {
+		if ( ! self::$config['fullscreen'] ) {
+			get_footer();
+			return;
+		}
+
+		add_filter( 'wp_footer', array( __CLASS__, 'add_explode_comment' ), 0 );
+
+		ob_start();
+		get_footer();
+		$footer = ob_get_contents();
+
+		ob_end_clean();
+
+		$elements = explode('<!--fullscreen-gallery-->', $footer );
+
+		echo $elements[1];
+	}
+
+	public static function add_explode_comment() {
+		echo '<!--fullscreen-gallery-->';
 	}
 
 }
--- templates/fullscreen.php
+++ templates/fullscreen.php
@@ -1,35 +1,32 @@
 <?php
+	Fullscreen_gallery::get_header();
 
-//get_header();
-
-echo '<html><head>';
-do_action('wp_head');
-
-$post      = get_post();
-$images    = get_post_galleries( $post, false );
-$image_ids = wp_list_pluck( $images,'ids' );
-$image_ids = explode(",", $image_ids[0]);
-
-echo '<div id="slides"> <div class="slides-container">';
-
-foreach ( $image_ids as $image_id ) {
-	echo wp_get_attachment_image( $image_id, 'large' );
-}
-
-echo '</div></div>';
-
+	$post      = get_post();
+	$images    = get_post_galleries( $post, false );
+	$image_ids = wp_list_pluck( $images,'ids' );
+	$image_ids = explode(",", $image_ids[0]);
 ?>
 
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$('#slides').superslides({
-			animation: "slide",
-			scrollable: false,
-			hashchange: true
-		})
-	});
-</script>
+		<div id="slides">
+			<div class="slides-container">
+
+				<?php
+				foreach ( $image_ids as $image_id ) {
+					echo wp_get_attachment_image( $image_id, 'large' ) . PHP_EOL . PHP_EOL;
+				}
+				?>
+			</div>
+		</div>
+
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$('#slides').superslides({
+					animation: "slide",
+					scrollable: false,
+					hashchange: true
+				})
+			});
+		</script>
 
 <?php
-echo '</body></html>';
-
+Fullscreen_gallery::get_footer();
